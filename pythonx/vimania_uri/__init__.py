23b76e97297463cfc8054a4b7fdeb68e6a0277bb
sysid <sysid@gmx.de>
2022-12-14 18:48:15 +0100

    Bump version: 0.8.1 → 0.9.0

--- pythonx/vimania_uri/__init__.py
+++ pythonx/vimania_uri/__init__.py
@@ -1,5 +1,5 @@
 # from vimania_uri.todos.cli import app
 # from vimania_uri.vim_.vimania_manager import VimaniaUriManager
 
-__version__ = "0.8.1"
+__version__ = "0.9.0"
 # __all__ = ["VimaniaUriManager", "app"]
